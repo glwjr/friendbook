f4beda32899594ba8222bf9cc0f4edce14ff50b9
Gary White <glw494@gmail.com>
2023-02-05 23:23:12 -0500

    added express urlencoded config

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 app.use('/public', express.static(path.join(__dirname, '../public')));
