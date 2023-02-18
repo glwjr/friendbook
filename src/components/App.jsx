1ac334f65bad917b6b5a7e834eebeea9effe2549
Gary White <glw494@gmail.com>
2023-02-18 16:34:16 -0500

    CSS baseline and formatting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  Link, Routes, Route, Navigate,
+  Routes, Route, Navigate,
 } from 'react-router-dom';
+import { Container, ScopedCssBaseline } from '@mui/material';
 import Home from './Home';
 import Login from './Login';
 import { fetchConversations, fetchPosts, loginWithToken } from '../store';
@@ -10,6 +11,7 @@ import Register from './Register';
 import Conversations from './Conversations';
 import Posts from './Posts';
 import NotFound from './NotFound';
+import ResponsiveAppBar from './ResponsiveAppBar';
 
 function App() {
   const { auth } = useSelector((state) => state);
@@ -27,20 +29,18 @@ function App() {
   }, [auth]);
 
   return (
-    <div className="app">
-      <nav>
-        {auth.id ? <Link to="/">Home</Link> : ''}
-        {auth.id ? <Link to="/posts">Posts</Link> : ''}
-        {auth.id ? <Link to="/conversations">Conversations</Link> : ''}
-      </nav>
-      <Routes>
-        <Route path="/" element={auth.id ? <Home /> : <Login />} />
-        <Route path="/register" element={auth.id ? <Navigate to="/" /> : <Register />} />
-        <Route path="/posts" element={auth.id ? <Posts /> : <NotFound />} />
-        <Route path="/conversations" element={auth.id ? <Conversations /> : <NotFound />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <ScopedCssBaseline>
+      <ResponsiveAppBar />
+      <Container maxWidth="xl">
+        <Routes>
+          <Route path="/" element={auth.id ? <Home /> : <Login />} />
+          <Route path="/register" element={auth.id ? <Navigate to="/" /> : <Register />} />
+          <Route path="/posts" element={auth.id ? <Posts /> : <Login />} />
+          <Route path="/conversations" element={auth.id ? <Conversations /> : <Login />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </ScopedCssBaseline>
   );
 }
 
